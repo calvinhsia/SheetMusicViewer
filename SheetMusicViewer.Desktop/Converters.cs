96a0d783ee6aa0e4e708d40c90b99c731fd1d874
Calvinh <Calvin_hsia@alum.mit.edu>
2025-12-21 02:52:30 +0000

    Add favorite column to TOC grid and support star display
    Added a "Fav" column to the TOC DataGrid to visually indicate favorite entries with a star. Updated view models to track and display favorites efficiently. Introduced a BoolToStarConverter for future binding needs. Includes minor refactoring and using directive updates.

---
+++ SheetMusicViewer.Desktop/Converters.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace SheetMusicViewer.Desktop;
+
+/// <summary>
+/// Converts boolean to star character for displaying favorites
+/// </summary>
+public class BoolToStarConverter : IValueConverter
+{
+    public static readonly BoolToStarConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue && boolValue)
+        {
+            return "★";
+        }
+        return string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // One-way converter - return unchanged value
+        return value;
+    }
+}
